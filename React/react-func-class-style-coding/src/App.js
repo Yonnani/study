24f568352bbf8850d0831364284b8f369eb670dc
Yonnani <iamai5206@gmail.com>
2020-10-16 00:43:33 +0900

    리액트 함수, 클래스에서 state 사용법(3-2)까지 수강

--- React/react-func-class-style-coding/src/App.js
+++ React/react-func-class-style-coding/src/App.js
@@ -1,29 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  console.log('app');
   return (
-    <div className="App">
+    <div className="container">
       <h1>Hello World</h1>
-      <FuncComp></FuncComp>
-      <ClassComp></ClassComp>
+      <FuncComp initNumber={2}></FuncComp>
+      <ClassComp initNumber={2}></ClassComp>
     </div>
   );
 }
 
-function FuncComp() {
+function FuncComp(props) {
+  var numberState = useState(props.initNumber);
+  var number = numberState[0];
+  var setNumber = numberState[1];
+  
+  console.log('numberState', numberState);
+  var [_date, setDate] = useState((new Date()).toString());
+  
   return (
     <div className="container">
       <h2>function style component</h2>
+      <p>Number : {number}</p>
+      <p>Date : {_date}</p>
+      <input 
+        type="button" value="random" 
+        onClick={
+          function() {
+            setNumber(Math.random());
+          }
+        }
+      />
+      <input
+        type="button" value="date"
+        onClick={
+          function() {
+            setDate((new Date()).toString());
+          }
+        }
+      />
     </div>
   );
 }
 
 class ClassComp extends React.Component {
+  state = {
+    number: this.props.initNumber,
+    date: (new Date()).toString()
+  }
   render() {
+    console.log('class component');
     return (
       <div className="container">
         <h2>class style component</h2>
+        <p>Number : {this.state.number}</p>
+        <p>Date : {this.state.date}</p>
+        <input type="button" value="random" 
+          onClick={
+            function() {
+              this.setState({number: Math.random()})
+            }.bind(this)
+          }
+        />
+        <input
+          type="button" value="date"
+          onClick={
+            function() {
+              this.setState({date: (new Date()).toString()});
+            }.bind(this)
+          }
+        />
       </div>
     );
   }
