61acb39be5f4559456bf4ad1f1c22b8bb9c61635
Yonnani <iamai5206@gmail.com>
2020-10-18 12:19:03 +0900

    리액트 클래스에서 라이프사이클 구현(4-1)까지 수강

--- React/react-func-class-style-coding/src/App.js
+++ React/react-func-class-style-coding/src/App.js
@@ -45,13 +45,30 @@ function FuncComp(props) {
   );
 }
 
+var classStyle = 'color: red';
 class ClassComp extends React.Component {
   state = {
     number: this.props.initNumber,
     date: (new Date()).toString()
   }
+  componentWillMount() {
+    console.log('%cclass => componentWillMount', classStyle);
+  }
+  componentDidMount() {
+    console.log('%cclass => componentDidMount', classStyle);
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('%cclass => shouldComponentUpdate', classStyle);
+    return true;
+  }
+  componentWillUpdate(nextProps, nextState) {
+    console.log('%cclass => componentWillUpdate', classStyle);
+  }
+  componentDidUpdate(nextProps, nextState) {
+    console.log('%cclass => componentDidUpdate', classStyle);
+  }
   render() {
-    console.log('class component');
+    console.log('%cclass => render', classStyle);
     return (
       <div className="container">
         <h2>class style component</h2>
