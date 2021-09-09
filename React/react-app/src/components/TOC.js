f090ade797db8e832ba133eb947465bf3cfde41c
Yonnani <iamai5206@gmail.com>
2021-09-10 01:21:38 +0900

    생활코딩 React - 17.3. 컴포넌트 이벤트까지 수강

---
+++ React/react-app/src/components/TOC.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class TOC extends Component {
+    render() {
+        var lists = [];
+        var data = this.props.data;
+        var i = 0;
+        while( i < data.length ) {
+            lists.push(
+                <li key={data[i].id}>
+                    <a 
+                        href={"/content/"+data[i].id}
+                        data-id={data[i].id}
+                        onClick={function(id, e){
+                            e.preventDefault();
+                            this.props.onChangePage(id);
+                        }.bind(this, data[i].id)}>
+                        {data[i].title}
+                    </a>
+                </li>);
+            i = i + 1;
+        }
+
+      return (
+        <nav>
+          <ul>
+            {lists}
+          </ul>
+        </nav>
+      );
+    }
+}
+
+export default TOC;
